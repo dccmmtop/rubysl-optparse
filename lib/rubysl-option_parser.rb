ab33570c0f675b93a83ad1a2dfa68b9e6fd524f5
Brian Shirai <brixen@gmail.com>
2013-04-14 17:14:51 -0700

    Created gem.

---
+++ lib/rubysl-option_parser.rb
@@ -0,0 +1,7 @@
+require "rubysl-option_parser/version"
+
+module RubySL
+  module OptionParser
+    # Your code goes here...
+  end
+end
---
+++ lib/rubysl-option_parser/version.rb
@@ -0,0 +1,5 @@
+module RubySL
+  module OptionParser
+    VERSION = "0.0.1"
+  end
+end
